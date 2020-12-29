3e2a848e90edb6591fc4782d164f5f4a6dd66fa1
Krystian <krystian.izdebski1999@gmail.com>
2020-12-30 00:47:41 +0100

    working example of using fetch in javascript

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 async function getText() {
   var myObject = await fetch("http://localhost:8080/RestApiPractising/webapi/student/get/2");
   var myText = await myObject.text();
-  document.getElementById("container").innerHTML = myText;
+  var object = JSON.parse(myText);
+  var toShow = "First name: " + object.firstName + " | Last name: " + object.lastName + " | age: " + object.age; 
+  document.getElementById("container").innerHTML = toShow;
 }
 
 window.onload = getText
